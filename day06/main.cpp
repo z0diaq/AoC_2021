d9964f3765e11f7090c8533ac510f4a06c8f5f9a
Piotr Tkaczyk <p.tkaczyk79@gmail.com>
2022-11-20 05:06:50 +0100

    day 6 done - only needed test data and results' update

--- day06/main.cpp
+++ day06/main.cpp
@@ -7,10 +7,10 @@ int main( )
     lanternfish::Result result;
 
     std::cout << "#### PART 1:" << std::endl;
-    auto resultPart1 = result.Execute( 5934        , 345387 );
+    auto resultPart1 = result.Execute( 5'934        , 359'344 );
 
     std::cout << std::endl << "#### PART 2:" << std::endl;
-    auto resultPart2 = result.Execute( 26984457539u, 1574445493136u, AoC::Stage::PART_TWO );
+    auto resultPart2 = result.Execute( 26'984'457'539u, 1'629'570'219'571u, AoC::Stage::PART_TWO );
 
     return
         resultPart1 == AoC::ResultType::FAILED ||
