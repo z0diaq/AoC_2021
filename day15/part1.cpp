2465d82286be65c7b5f5251d54d0bb0169be29db
Piotr Tkaczyk <p.tkaczyk79@gmail.com>
2025-01-25 23:39:17 +0100

    day 15 - A* wip (2)

--- day15/part1.cpp
+++ day15/part1.cpp
@@ -47,17 +47,46 @@ bool operator<( const PositionWithWeight& _lhs, const PositionWithWeight& _rhs )
 	return _lhs.m_weight < _rhs.m_weight;
 }
 
+
+bool SamePosition( const PositionWithWeight& _lhs, const PositionWithWeight& _rhs );
+
 std::size_t ShortestPathLength( const std::deque<std::string>& _map )
 {
+	// direct implementation of https://en.wikipedia.org/wiki/A*_search_algorithm
+
+	const std::uint32_t width{ static_cast< std::uint32_t >( _map.front( ).length( ) ) };
+	const std::uint32_t height{ static_cast< std::uint32_t >( _map.size( ) ) };
 
 	std::priority_queue< PositionWithWeight> openSet;
-	openSet.push( PositionWithWeight{ 0, 0, static_cast<unsigned int>(_map[ 0 ][ 0 ]) });
+	openSet.push( PositionWithWeight{ 0, 0, static_cast< unsigned int >( _map[ 0 ][ 0 ] ) } );
 
+	std::map<PositionWithWeight, PositionWithWeight> cameFrom;
 
+	using WeightsRow = std::vector<std::uint64_t>;
+	using WeightsMap = std::vector<WeightsRow>;
 
+	WeightsRow rowInit( width, std::numeric_limits<std::uint64_t>::max( ) );
+	WeightsMap gScore( height, rowInit );
 
+	WeightsMap fScore = gScore;
+	fScore[ 0 ][ 0 ] = 1;// don't use heuristics since graph is weighted
 
+	const PositionWithWeight destination{ height - 1, width - 1, 0 };
 
+	while( false == openSet.empty( ) )
+	{
+		auto current = openSet.top( );
+		openSet.pop( );
+
+		if( SamePosition( current, destination ) )
+			return current.m_weight;
+	}
 
 	return 0;
 }
+
+bool SamePosition( const PositionWithWeight& _lhs, const PositionWithWeight& _rhs )
+{
+	return _lhs.m_x == _rhs.m_x && _lhs.m_y == _rhs.m_y;
+}
+
