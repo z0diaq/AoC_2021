8d086db8a07d0a319b658c06b7e5b8a0960294c3
Piotr Tkaczyk <p.tkaczyk79@gmail.com>
2025-01-27 23:53:38 +0100

    day 13 cleanup

--- day13/part1.cpp
+++ day13/part1.cpp
@@ -1,22 +1,8 @@
 import transparent_origami;
 
 //leave what is needed
-#include <iostream>
 #include <string>
 #include <algorithm>
-#include <stdexcept>
-
-//containers
-#include <vector>
-#include <map>
-#include <set>
-#include <deque>
-#include <array>
-
-//boost
-#include <boost/algorithm/string/split.hpp>
-#include <boost/algorithm/string/classification.hpp>
-#include <boost/algorithm/string/replace.hpp>
 
 using namespace transparent_origami;
 
