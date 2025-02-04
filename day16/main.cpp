653c49a37d3fd7585aea7b8884234658de546ecc
Piotr Tkaczyk <p.tkaczyk79@gmail.com>
2025-02-04 23:56:11 +0100

    day 16 - basic GTest integration

--- day16/main.cpp
+++ day16/main.cpp
@@ -1,8 +1,12 @@
 import packet_decoder;
 
 #include <compare>
+#include <gtest/gtest.h>
 
 int main(int argc, char* argv[])
 {
+	testing::InitGoogleTest( &argc, argv );
+	if( RUN_ALL_TESTS( ) )
+		return 1;
 	return static_cast< int >( packet_decoder::Result().Execute(argc, argv));
 }
