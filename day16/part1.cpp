e53ef4b5cf3a1d69d186e52a6d0bb27816e910e1
Piotr Tkaczyk <p.tkaczyk79@gmail.com>
2025-02-05 20:54:59 +0100

    day 16 - part 1 wip added UT + fixes 2/3 part 1 done

--- day16/part1.cpp
+++ day16/part1.cpp
@@ -20,7 +20,11 @@ import packet_decoder;
 #include <boost/lexical_cast.hpp>
 #include <boost/algorithm/string/replace.hpp>
 
+#include <gtest/gtest.h>
+
 using namespace packet_decoder;
+
+class BinaryHolder;
 
 void
 Result::ProcessOne( const std::string& data )
@@ -28,7 +32,7 @@ Result::ProcessOne( const std::string& data )
 	m_packet = data;
 }
 
-void DecodePacket( const std::string& packet );
+void DecodePacket( BinaryHolder& holder );
 
 std::string
 Result::FinishPartOne( )
@@ -52,36 +56,43 @@ public:
 
 	unsigned int GetNextChunk( size_t bitsCount )
 	{
-		unsigned int result{ 0 };
-		if( m_currentPosition + bitsCount >= m_binaryData.size( ) )
-			throw std::logic_error( "Index out of bounds!" );
-
-		result = std::bitset<10>( m_binaryData.substr( m_currentPosition, bitsCount ) ).to_ulong( );
-		m_currentPosition += bitsCount;
-		return result;
+		return std::bitset<10>( GetNextStringChunk( bitsCount ) ).to_ulong( );
 	}
 
 	std::string GetNextStringChunk( size_t bitsCount )
 	{
-		if( m_currentPosition + bitsCount >= m_binaryData.size( ) )
+		if( m_currentPosition + bitsCount > m_binaryData.size( ) )
+		{
+			std::cout << "current pos" << m_currentPosition << " + " << bitsCount << " vs size=" << m_binaryData.size( ) << std::endl;
 			throw std::logic_error( "Index out of bounds!" );
+		}
 
-		return m_binaryData.substr( m_currentPosition, bitsCount );
+		std::string result = m_binaryData.substr( m_currentPosition, bitsCount );
+		m_currentPosition += bitsCount;
+		return result;
+	}
+
+	void AdjustPadding( )
+	{
+		const unsigned int beyondPaddingPosition{ m_currentPosition % 4 };
+		if( beyondPaddingPosition )
+			m_currentPosition += ( 4 - beyondPaddingPosition );
 	}
 };
 
 unsigned int ParseLiteralValue( BinaryHolder& holder );
+unsigned int ParseOperator( BinaryHolder& holder );
 
-void DecodePacket( const std::string& packet )
+void DecodePacket( BinaryHolder& holder )
 {
-	BinaryHolder holder( HexToBinary( packet ) );
+	
 
 	unsigned int versionNumber = holder.GetNextChunk( 3 );
 	unsigned int typeID = holder.GetNextChunk( 3 );
 	if( typeID == 4 )
 		unsigned int literalValue = ParseLiteralValue( holder );
 	else
-		;
+		unsigned int operatorValue = ParseOperator( holder );
 }
 
 std::string HexToBinary( const std::string& str )
@@ -89,11 +100,11 @@ std::string HexToBinary( const std::string& str )
 	std::string binaryResult;
 
 	binaryResult.reserve( str.length( ) * 4 );
-	static const char* hexMap = "000010001100110011001100110011001100110011001100";
+	static const char* hexMap = "0000000100100011010001010110011110001001101010111100110111101111";
 
-	for( char c : str ) {
-		unsigned idx = ( unsigned )( c & 0xF );
-		binaryResult.append( hexMap + ( idx << 1 ), 4 );
+	for( char hex : str ) {
+		unsigned idx = ( hex >= '0' && hex <= '9' ) ? ( hex - '0' ) : 10 + ( hex - 'A' );
+		binaryResult.append( hexMap + ( idx * 4 ), 4 );
 	}
 
 	return binaryResult;
@@ -101,15 +112,118 @@ std::string HexToBinary( const std::string& str )
 
 unsigned int ParseLiteralValue( BinaryHolder& holder )
 {
-	constexpr unsigned int notLastGroupMask = ( 1 << 4 );
-	unsigned int nextChunk{ 0 };
-	unsigned int result{ 0 };
-	std::string result;
-	do
+	std::string finalValue;
+	while( true )
 	{
-		nextChunk = holder.GetNextChunk( 5 );
+		std::string nextChunk{ holder.GetNextStringChunk( 5 ) };
+		finalValue.append( nextChunk.substr( 1 ) );
+		if( nextChunk.front( ) == '0' )
+			break;
+	}
 
+	std::cout << "final LiteralValue string = " << finalValue << std::endl;
 
-	} while( nextChunk & notLastGroupMask );
+	if( finalValue.length( ) > 30 )
+	{
+		std::cerr << "FinalValue has " << finalValue.length( ) << " characters!" << std::endl;
+		throw std::logic_error( "Need to use bigger bitset!" );
+	}
+	holder.AdjustPadding( );
+	return std::bitset<30>( finalValue ).to_ulong( );
 }
 
+
+unsigned int ParseOperator( BinaryHolder& holder )
+{
+	unsigned int lengthTypeID = holder.GetNextChunk( 1 );
+
+	if( lengthTypeID == 0 )
+	{
+		unsigned int totalLengthInBits = holder.GetNextChunk( 15 );
+	}
+	else
+	{
+		unsigned int numberOfSubPackets = holder.GetNextChunk( 11 );
+
+	}
+}
+
+
+
+
+
+
+using namespace testing;
+
+
+TEST( GetNextChunk, CheckProcessing_D2FE28 )
+{
+	BinaryHolder holder( HexToBinary( "D2FE28" ) );
+
+	ASSERT_EQ( holder.GetNextChunk( 3 ), 6 );
+	ASSERT_EQ( holder.GetNextChunk( 3 ), 4 );
+	ASSERT_EQ( holder.GetNextChunk( 5 ), 23 ); // 10111
+	ASSERT_EQ( holder.GetNextChunk( 5 ), 30 ); // 11110
+	ASSERT_EQ( holder.GetNextChunk( 5 ), 5 );  // 00101
+	ASSERT_EQ( holder.GetNextChunk( 3 ), 0 );  //   000 padding
+}
+
+
+struct TestScenario
+{
+	std::string m_hexString;
+	std::string m_expectedResult;
+	std::string m_description;
+};
+
+class HexToBinaryTest : public TestWithParam<TestScenario>
+{
+protected:
+	void SetUp( ) override
+	{
+		m_scenario = GetParam( );
+	}
+
+	TestScenario m_scenario;
+};
+
+TEST_P( HexToBinaryTest, BasicValues )
+{
+	auto binaryString = HexToBinary( m_scenario.m_hexString );
+	ASSERT_EQ( binaryString, std::string( m_scenario.m_expectedResult ));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+	HexToBinaryTests,
+	HexToBinaryTest,
+	::testing::Values(
+		TestScenario{ "0", "0000", "Zero" },
+		TestScenario{ "1", "0001", "One" },
+		TestScenario{ "4", "0100", "Four" },
+		TestScenario{ "5", "0101", "Five" },
+		TestScenario{ "6", "0110", "Size" },
+		TestScenario{ "7", "0111", "Seven" },
+		TestScenario{ "8", "1000", "Eight" },
+		TestScenario{ "9", "1001", "Nine" },
+		TestScenario{ "A", "1010", "A" },
+		TestScenario{ "B", "1011", "B" },
+		TestScenario{ "C", "1100", "C" },
+		TestScenario{ "D", "1101", "D" },
+		TestScenario{ "E", "1110", "E" },
+		TestScenario{ "F", "1111", "F" },
+		TestScenario{ "D2FE28", "110100101111111000101000", "D2FE28" }
+	),
+[ ]( const testing::TestParamInfo<HexToBinaryTest::ParamType>& p ) { return p.param.m_description; }
+);
+
+TEST( ParseLiteralValue, D2FE28 )
+{
+	BinaryHolder holder( HexToBinary( "D2FE28" ) );
+
+	// skip first 6 bits - they are of no concern to us
+	holder.GetNextChunk( 6 );//result ignored
+
+	unsigned int literalValue = ParseLiteralValue( holder );
+
+	ASSERT_EQ( literalValue, 2021 );
+}
