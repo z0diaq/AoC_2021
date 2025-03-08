39a9a8e23b4b6b8a1cf8d3e407485d4331c46c7e
Piotr Tkaczyk <p.tkaczyk79@gmail.com>
2025-03-08 20:53:19 +0100

    day 21 part 2 - wip - memoization prototype

--- day21/part2.cpp
+++ day21/part2.cpp
@@ -22,12 +22,49 @@ import dirac_dice;
 using namespace dirac_dice;
 
 void
-Result::ProcessTwo(const std::string& data)
+Result::ProcessTwo(const std::string& _data)
 {
+	ProcessOne( _data );
 }
+
+struct GameState
+{
+	PlayerInfo m_player1;
+	PlayerInfo m_player2;
+	std::uint32_t m_player2Score{ };
+	bool m_isPlayer1Turn{ };
+
+	std::size_t GetHash( ) const
+	{
+		std::size_t p1pHash{ std::hash<int>{}( m_player1.m_position ) };
+		std::size_t p1sHash{ std::hash<int>{}( m_player1.m_score ) };
+		std::size_t p2pHash{ std::hash<int>{}( m_player2.m_position ) };
+		std::size_t p2sHash{ std::hash<int>{}( m_player2.m_score ) };
+		std::size_t p1tHash{ std::hash<bool>{}( m_isPlayer1Turn ) };
+
+		// one of many possible hash combining methods
+		return p1pHash ^ ( p1sHash << 1 ) ^ ( p2pHash << 2 ) ^ ( p2sHash << 3 ) ^ ( p1tHash << 4 );
+	}
+};
+
+struct GameStateHash {
+	std::size_t
+	operator()( const GameState& _state ) const
+	{
+		return _state.GetHash( );
+	}
+};
 
 std::string
 Result::FinishPartTwo()
 {
+	using Wins = std::array<std::uint64_t, 2>;
+
+	constexpr std::uint32_t winScore{ 21 };
+
+	std::unordered_map<GameState, Wins, GameStateHash> memoization;
+
+	GameState initialState{ m_players[ 0 ], m_players[ 1 ] };
+
 	return std::to_string(0);
 }
